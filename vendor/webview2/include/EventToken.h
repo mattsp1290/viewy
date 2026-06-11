4d10ae11aaa8c430b199c9e77b4fcb03d3130198
Matt Spurlin <mattsp1290@gmail.com>
2026-06-11 15:22:42 -0400

    ralph: iteration 14 - fix webview2 event token shim

--- vendor/webview2/include/EventToken.h
+++ vendor/webview2/include/EventToken.h
@@ -1,5 +1,14 @@
 #pragma once
 
 /* WebView2.h includes "EventToken.h". Some MinGW-w64 SDKs provide the
-   Windows SDK header as lowercase <eventtoken.h> on case-sensitive filesystems. */
+   Windows SDK header as lowercase <eventtoken.h> on case-sensitive filesystems.
+   Current hosted Windows images expose an eventtoken.h that does not make the
+   WinRT token type visible to this vendored WebView2 header, so keep this shim
+   self-contained. */
 #include <eventtoken.h>
+
+#ifndef EventRegistrationToken
+typedef struct EventRegistrationToken {
+  __int64 value;
+} EventRegistrationToken;
+#endif
