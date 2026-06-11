960d81fa0b6a528c96b00c88390ee60a1be1136b
Matt Spurlin <mattsp1290@gmail.com>
2026-06-11 13:56:17 -0400

    ralph: iteration 7 checkpoint - vendor webview2 headers

---
+++ vendor/webview2/include/EventToken.h
@@ -0,0 +1,5 @@
+#pragma once
+
+/* WebView2.h includes "EventToken.h". Some MinGW-w64 SDKs provide the
+   Windows SDK header as lowercase <eventtoken.h> on case-sensitive filesystems. */
+#include <eventtoken.h>
